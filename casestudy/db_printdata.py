715758fef05a56004b8962e3a3d48de78a6c9ac9
James Beck <jjbeck97@gmail.com>
2022-12-07 11:27:12 -0500

    module 8 assignment done

--- casestudy/db_printdata.py
+++ casestudy/db_printdata.py
@@ -38,43 +38,54 @@ cursor = db.cursor()
 # Inventory
 cursor.execute("""SELECT * FROM inventory""")
 inventory = cursor.fetchall()
+
+print(" -- INVENTORY --")
 for item in inventory:
-    print(item)
+    print(f"{item[0]}\tQuantity: {item[1]}\tLocation: {item[2]}")
 
 # Products
 cursor.execute("""SELECT * FROM products""")
 products = cursor.fetchall()
+print("\n -- PRODUCTS --")
 for item in products:
-    print(item)
+    print(f'{item[0]}\tName: {item[1]}\tCost: ${item[2]}\tManufacturer: {item[3]}\tRental Cost: ${item[5]}')
 
 # Locations
 cursor.execute("""SELECT * FROM locations""")
 locations = cursor.fetchall()
+print("\n -- LOCATIONS --")
 for item in locations:
-    print(item)
+    print(f"{item[0]}\t{item[1]}")
 
 # Employees
 cursor.execute("""SELECT * FROM employees""")
 employees = cursor.fetchall()
+print("\n -- EMPLOYEES --")
 for item in employees:
-    print(item)
+    print(f"{item[0]}\tName: {item[1]}\tDepartment: {item[2]}")
 
 # Trips
 cursor.execute("""SELECT * FROM trips""")
 trips = cursor.fetchall()
+print("\n -- TRIPS --")
 for item in trips:
-    print(item)
+    print(f"{item[0]}\tEmployee ID: {item[1]}\tLocation ID: {item[2]}\tAirport: {item[3]}\tAirfare: ${item[4]}"
+          f"\tDepert Date: {item[5]}\tReturn Date: {item[6]}\tInnoculations: {item[7]}")
 
 # Customers
 cursor.execute("""SELECT * FROM customers""")
 customers = cursor.fetchall()
+print("\n -- CUSTOMERS --")
 for item in customers:
-    print(item)
+    print(f"{item[0]}\tName: {item[1]}\tAddress: {item[2]} {item[3]}, {item[4]} {item[5]}\tEmail: {item[6]}\t"
+          f"Phone Number: {item[7]}\tTrip ID: {item[8]}")
 
 # Orders
 cursor.execute("""SELECT * FROM orders""")
 orders = cursor.fetchall()
+print("\n -- ORDERS --")
 for item in orders:
-    print(item)
+    print(f"{item[0]}\tCustomer ID: {item[0]}\tCustomer ID: {item[1]}\tOrder Date: {item[2]}\tTotal: ${item[3]}"
+          f"\tProduct ID: {item[4]}\tRental Start Date: {item[6]}\tRental End Date: {item[7]}\tTotal: ${item[8]}")
 
 db.close()
